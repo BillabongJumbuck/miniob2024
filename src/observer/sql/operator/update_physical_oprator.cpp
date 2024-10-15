5eb8324edc4e208d6afc24fae2cb46cc476b34ee
QinJM <2792904471@qq.com>
2024-10-15 23:01:20 +0800

    qjm-update-test

---
+++ src/observer/sql/operator/update_physical_oprator.cpp
@@ -0,0 +1,74 @@
+//
+// Created by qjm on 10/14/24.
+//
+
+#include "sql/operator/update_physical_oprator.h"
+
+#include <utility>
+#include "common/log/log.h"
+#include "storage/table/table.h"
+#include "storage/trx/trx.h"
+
+UpdatePhysicalOperator::UpdatePhysicalOperator(Table *table,const FieldMeta *field_meta, Value value)
+{
+  table_ = table;
+  field_meta_ = field_meta;
+  value_ = std::move(value);
+}
+
+RC UpdatePhysicalOperator::open(Trx *trx)
+{
+  if (children_.empty()) {
+    return RC::SUCCESS;
+  }
+
+  std::unique_ptr<PhysicalOperator> &child = children_[0];
+
+  RC rc = child->open(trx);
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to open child operator: %s", strrc(rc));
+    return rc;
+  }
+
+  trx_ = trx;
+
+  while (OB_SUCC(rc = child->next())) {
+    Tuple *tuple = child->current_tuple();
+    if (nullptr == tuple) {
+      LOG_WARN("failed to get current record: %s", strrc(rc));
+      return rc;
+    }
+
+    RowTuple *row_tuple = static_cast<RowTuple *>(tuple);
+    Record   &record    = row_tuple->record();
+    records_.emplace_back(std::move(record));
+  }
+
+  child->close();
+
+  // 先收集记录再Update
+  // 记录的有效性由事务来保证，如果事务不保证Update的有效性，那说明此事务类型不支持并发控制，比如VacuousTrx
+  for (Record &record : records_) {
+    size_t       copy_len = field_meta_->len();
+    const size_t data_len = value_.length();
+    if (field_meta_->type() == AttrType::CHARS) {
+      if (copy_len > data_len) {
+        copy_len = data_len + 1;
+      }
+    }
+    memcpy(record.data() + field_meta_->offset(), value_.data(), copy_len);
+
+    // if (rc != RC::SUCCESS) {
+    //   LOG_WARN("failed to update record: %s", strrc(rc));
+    //   return rc;
+    // }
+  }
+
+  return RC::SUCCESS;
+}
+
+RC UpdatePhysicalOperator::next() { return RC::RECORD_EOF; }
+
+RC UpdatePhysicalOperator::close() { return RC::SUCCESS; }
+
+
--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -12,15 +12,80 @@ See the Mulan PSL v2 for more details. */
 // Created by Wangyunlai on 2022/5/22.
 //
 
+#include "common/log/log.h"
+#include "common/types.h"
+#include "common/lang/string.h"
+#include "sql/stmt/filter_stmt.h"
 #include "sql/stmt/update_stmt.h"
+#include "storage/db/db.h"
 
-UpdateStmt::UpdateStmt(Table *table, Value *values, int value_amount)
-    : table_(table), values_(values), value_amount_(value_amount)
-{}
+#include <storage/table/table.h>
+
+UpdateStmt::UpdateStmt(Table *table, const FieldMeta *field_meta, Value value,  FilterStmt *filter_stmt)
+{
+  table_ = table;
+  field_meta_ = field_meta;
+  value_ = value;
+  filter_stmt_ = filter_stmt;
+}
+
+UpdateStmt::~UpdateStmt()
+{
+  if (nullptr != filter_stmt_) {
+    delete filter_stmt_;
+    filter_stmt_ = nullptr;
+  }
+}
 
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
-  // TODO
-  stmt = nullptr;
-  return RC::INTERNAL;
+  const char *table_name = update.relation_name.c_str();
+  if (nullptr == db || nullptr == table_name) {
+    LOG_WARN("invalid argument. db=%p, table_name=%p",
+        db, table_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // check whether the table exists
+  Table *table = db->find_table(table_name);
+  if (nullptr == table) {
+    LOG_WARN("no such table. db=%s, table_name=%s", db->name(), table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  // 检查属性名是否为空
+  const char *attr_name = update.attribute_name.c_str();
+  if(common::is_blank(attr_name)) {
+    LOG_WARN("invalid argument. db=%p, attr_name=%p", db, attr_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // 检查属性名是否存在
+  const FieldMeta *field_meta = table->table_meta().field(attr_name);
+  if(field_meta == nullptr) {
+    LOG_WARN("update table error :no such attribute. db=%s, attr_name=%s", db->name(), attr_name);
+    return RC::SCHEMA_FIELD_NOT_EXIST;
+  }
+
+  // 检查类型是否一致
+  Value value = update.value;
+  if(value.attr_type() != field_meta->type()) {
+    LOG_WARN("invalid argument. db=%p, attr_name=%p", db, attr_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // 创建条件查询语句
+  std::unordered_map<std::string, Table *> table_map;
+  table_map.insert(std::pair<std::string, Table *>(std::string(table_name), table));
+
+  FilterStmt *filter_stmt = nullptr;
+  RC          rc          = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
+    return rc;
+  }
+
+  stmt = new UpdateStmt(table, field_meta, value, filter_stmt);
+  return RC::SUCCESS;
 }
