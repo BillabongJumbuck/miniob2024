5de5c6b8ec84829705ef6a00686b212b0b5f6ac6
QinJM <2792904471@qq.com>
2024-10-15 23:01:20 +0800

    qjm-update-test

---
+++ src/observer/sql/operator/update_logical_operator.cpp
@@ -0,0 +1,13 @@
+//
+// Created by qjm on 10/14/24.
+//
+
+#include "sql/operator/update_logical_operator.h"
+
+ UpdateLogicalOperator::UpdateLogicalOperator(Table *table, const FieldMeta *field_meta, Value value)
+ {
+   table_ = table;
+   field_meta_ = field_meta;
+   value_ = value;
+ }
+
